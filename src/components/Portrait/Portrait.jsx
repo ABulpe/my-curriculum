2bf0605f58f058181679dbf219de4bd7af4f7bf3
kelsier94 <abulpe.alumno@iesrafaelalberti.es>
2022-07-24 17:31:44 +0200

    Image

--- src/components/Portrait/Portrait.jsx
+++ src/components/Portrait/Portrait.jsx
@@ -3,7 +3,7 @@ import {BsLinkedin, BsFillHeartFill} from "react-icons/bs"
 
 
 const Portrait = () =>{
-    let img = "/storage/img/me.png"
+    let img = "/storage/img/me2.png"
     return (
         <>
             <Card>
