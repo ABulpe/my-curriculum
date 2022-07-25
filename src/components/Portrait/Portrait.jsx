720d289498ed1304a45a6f5af06f52645b1d9ec8
kelsier94 <alberto.bulpe@applieditweb.com>
2022-07-25 08:21:28 +0200

    image

--- src/components/Portrait/Portrait.jsx
+++ src/components/Portrait/Portrait.jsx
@@ -3,7 +3,7 @@ import {BsLinkedin, BsFillHeartFill} from "react-icons/bs"
 
 
 const Portrait = () =>{
-    let img = "/storage/img/me4.png"
+    let img = "/storage/img/me4.jpeg"
     return (
         <>
             <Card>
